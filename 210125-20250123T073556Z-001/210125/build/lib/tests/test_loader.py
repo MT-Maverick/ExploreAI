949f8fe023dc95aca7a4fcea32c1de17c7910695
MT-Maverick <mfundosindane0@gmail.com>
2025-01-24 12:56:48 +0000

    Removed all documents starting anew

--- 210125-20250123T073556Z-001/210125/build/lib/tests/test_loader.py
+++
@@ -1,6 +0,0 @@
-from weather_analysis.loader import load_data
-
-def test_load_data():
-    file_path =  'C:\\Users\\HP\\Downloads\\trial\\chart.csv' #change this to your path
-    data = load_data(file_path)
-    assert not data.empty, "Data should not be empty"
--- 210125-20250123T073556Z-001/210125/tests/test_stats.py
+++
@@ -1,9 +0,0 @@
-from weather_analysis.stats import column_stats
-import pandas as pd
-
-def test_column_stats():
-    sample_data = pd.DataFrame({'min_temp': [10,11,12,9,8]})
-    stats = column_stats(sample_data, 'min_temp')
-    assert stats['mean'] == 10
-    assert stats['median'] == 10
-    assert stats['std_dev'] > 0
